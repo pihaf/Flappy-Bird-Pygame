46d2d4460afd9620536c78d6e6cb01946bd2aa1b
pihaf <113176707+pihaf@users.noreply.github.com>
2024-08-01 14:38:05 +0700

    feat: game menu

--- game.py
+++ game.py
@@ -4,25 +4,27 @@ import time
 from pygame.locals import *
 
 # game configurations
-SCREEN_WIDHT = 400
+SCREEN_WIDTH = 800 
 SCREEN_HEIGHT = 600
 SPEED = 20
 GRAVITY = 2.5
 GAME_SPEED = 15
 
-GROUND_WIDHT = 2 * SCREEN_WIDHT
+GROUND_WIDTH = 2 * SCREEN_WIDTH
 GROUND_HEIGHT = 100
 
-PIPE_WIDHT = 80
+PIPE_WIDTH = 60
 PIPE_HEIGHT = 500
-
+PIPE_DISTANCE = 250
 PIPE_GAP = 150
+START_PIPE_POS = 600
+
+CLOCK_TICK_RATE = 18
 
 wing = 'data/audio/wing.wav'
 hit = 'data/audio/hit.wav'
 
 pygame.mixer.init()
-
 
 class Bird(pygame.sprite.Sprite):
 
@@ -30,19 +32,17 @@ class Bird(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.images = [pygame.image.load('data/sprites/bluebird-upflap.png').convert_alpha(),
-                       pygame.image.load(
-            'data/sprites/bluebird-midflap.png').convert_alpha(),
-            pygame.image.load('data/sprites/bluebird-downflap.png').convert_alpha()]
+                       pygame.image.load('data/sprites/bluebird-midflap.png').convert_alpha(),
+                       pygame.image.load('data/sprites/bluebird-downflap.png').convert_alpha()]
 
         self.speed = SPEED
 
         self.current_image = 0
-        self.image = pygame.image.load(
-            'data/sprites/bluebird-upflap.png').convert_alpha()
+        self.image = pygame.image.load('data/sprites/bluebird-upflap.png').convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
 
         self.rect = self.image.get_rect()
-        self.rect[0] = SCREEN_WIDHT / 6
+        self.rect[0] = SCREEN_WIDTH / 6
         self.rect[1] = SCREEN_HEIGHT / 2
 
     def update(self):
@@ -60,16 +60,13 @@ class Bird(pygame.sprite.Sprite):
         self.current_image = (self.current_image + 1) % 3
         self.image = self.images[self.current_image]
 
-
 class Pipe(pygame.sprite.Sprite):
 
     def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
 
-        self. image = pygame.image.load(
-            'data/sprites/pipe-green.png').convert_alpha()
-        self.image = pygame.transform.scale(
-            self.image, (PIPE_WIDHT, PIPE_HEIGHT))
+        self. image = pygame.image.load('data/sprites/pipe-green.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (PIPE_WIDTH, PIPE_HEIGHT))
 
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
@@ -85,14 +82,12 @@ class Pipe(pygame.sprite.Sprite):
     def update(self):
         self.rect[0] -= GAME_SPEED
 
-
 class Ground(pygame.sprite.Sprite):
 
     def __init__(self, xpos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('data/sprites/base.png').convert_alpha()
-        self.image = pygame.transform.scale(
-            self.image, (GROUND_WIDHT, GROUND_HEIGHT))
+        self.image = pygame.transform.scale(self.image, (GROUND_WIDTH, GROUND_HEIGHT))
 
         self.mask = pygame.mask.from_surface(self.image)
 
@@ -103,10 +98,8 @@ class Ground(pygame.sprite.Sprite):
     def update(self):
         self.rect[0] -= GAME_SPEED
 
-
 def is_off_screen(sprite):
     return sprite.rect[0] < -(sprite.rect[2])
-
 
 def get_random_pipes(xpos):
     size = random.randint(100, 300)
@@ -114,13 +107,12 @@ def get_random_pipes(xpos):
     pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
     return pipe, pipe_inverted
 
-
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Flappy Bird')
 
 BACKGROUND = pygame.image.load('data/sprites/background-day.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDHT, SCREEN_HEIGHT))
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 BEGIN_IMAGE = pygame.image.load('data/sprites/message.png').convert_alpha()
 
 bird_group = pygame.sprite.Group()
@@ -130,23 +122,87 @@ bird_group.add(bird)
 ground_group = pygame.sprite.Group()
 
 for i in range(2):
-    ground = Ground(GROUND_WIDHT * i)
+    ground = Ground(GROUND_WIDTH * i)
     ground_group.add(ground)
 
 pipe_group = pygame.sprite.Group()
-for i in range(2):
-    pipes = get_random_pipes(SCREEN_WIDHT * i + 800)
+temp = START_PIPE_POS
+for i in range(6):
+    pipes = get_random_pipes(temp)  
     pipe_group.add(pipes[0])
     pipe_group.add(pipes[1])
-
+    temp += PIPE_DISTANCE
 
 clock = pygame.time.Clock()
+
+def draw_text(surface, text, size, x, y):
+    font = pygame.font.SysFont("comicsansms", size)
+    text_surface = font.render(text, True, (0, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surface.blit(text_surface, text_rect)
+
+def draw_menu():
+    options = [("Play", SCREEN_HEIGHT / 2), 
+               ("Settings", SCREEN_HEIGHT / 2 + 50), 
+               ("Quit", SCREEN_HEIGHT / 2 + 100)]
+    for text, y in options:
+        pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH / 2 - 100, y - 20, 200, 40))
+        pygame.draw.rect(screen, (0, 0, 0), (SCREEN_WIDTH / 2 - 100, y - 20, 200, 40), 2)
+        draw_text(screen, text, 32, SCREEN_WIDTH / 2, y - 20)
+
+def animate_click(option_rect, text):
+    for _ in range(1):
+        pygame.draw.rect(screen, (200, 200, 200), option_rect)
+        draw_text(screen, text, 32, SCREEN_WIDTH / 2, option_rect[1])
+        pygame.display.update()
+        pygame.time.delay(50)
+        pygame.draw.rect(screen, (255, 255, 255), option_rect)
+        pygame.draw.rect(screen, (0, 0, 0), option_rect, 2)
+        draw_text(screen, text, 32, SCREEN_WIDTH / 2, option_rect[1])
+        pygame.display.update()
+        pygame.time.delay(50)
+
+def main_menu():
+    menu = True
+    while menu:
+
+        clock.tick(CLOCK_TICK_RATE)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if SCREEN_WIDTH / 2 - 100 <= mouse_x <= SCREEN_WIDTH / 2 + 100:
+                    if SCREEN_HEIGHT / 2 - 20 <= mouse_y <= SCREEN_HEIGHT / 2 + 20:
+                        animate_click(pygame.Rect(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 20, 200, 40), "Play")
+                        menu = False
+                    elif SCREEN_HEIGHT / 2 + 30 <= mouse_y <= SCREEN_HEIGHT / 2 + 70:
+                        animate_click(pygame.Rect(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 + 30, 200, 40), "Settings")
+                        print("Settings clicked")  # Placeholder for settings functionality
+                    elif SCREEN_HEIGHT / 2 + 80 <= mouse_y <= SCREEN_HEIGHT / 2 + 120:
+                        animate_click(pygame.Rect(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 + 80, 200, 40), "Quit")
+                        pygame.quit()
+                        exit()
+
+        screen.blit(BACKGROUND, (0, 0))
+        bird.begin()
+        ground_group.update()
+        bird_group.draw(screen)
+        ground_group.draw(screen)
+        draw_menu()
+        pygame.display.update()
+
+
+main_menu()
 
 begin = True
 
 while begin:
 
-    clock.tick(15)
+    clock.tick(CLOCK_TICK_RATE)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -159,12 +215,12 @@ while begin:
                 begin = False
 
     screen.blit(BACKGROUND, (0, 0))
-    screen.blit(BEGIN_IMAGE, (120, 150))
+    screen.blit(BEGIN_IMAGE, ((SCREEN_WIDTH - BEGIN_IMAGE.get_width()) // 2, 150))  # Centered BEGIN_IMAGE
 
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
 
-        new_ground = Ground(GROUND_WIDHT - 20)
+        new_ground = Ground(GROUND_WIDTH - 20)
         ground_group.add(new_ground)
 
     bird.begin()
@@ -175,10 +231,9 @@ while begin:
 
     pygame.display.update()
 
-
 while True:
 
-    clock.tick(15)
+    clock.tick(CLOCK_TICK_RATE)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -194,14 +249,14 @@ while True:
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
 
-        new_ground = Ground(GROUND_WIDHT - 20)
+        new_ground = Ground(GROUND_WIDTH - 20)
         ground_group.add(new_ground)
 
     if is_off_screen(pipe_group.sprites()[0]):
         pipe_group.remove(pipe_group.sprites()[0])
         pipe_group.remove(pipe_group.sprites()[0])
 
-        pipes = get_random_pipes(SCREEN_WIDHT * 2)
+        pipes = get_random_pipes(SCREEN_WIDTH * 2)
 
         pipe_group.add(pipes[0])
         pipe_group.add(pipes[1])
